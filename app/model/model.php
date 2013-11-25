b3e7ac4041ff050252654be793be6f14a4fab71c
fabcinova <fabcinovad@gmail.com>
2013-11-25 23:46:52 +0100

    a

---
+++ app/model/model.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Description of model
+ *
+ * @author dominika
+ */
+class model extends Nette\Object{
+
+    protected $ID;
+    protected $ID_lekarny;
+    
+    public function __construct() {
+        // DB select
+    }
+    
+    public function create()
+    {
+        // DB insert
+    }
+
+    public function save()
+    {
+        // DB update
+    } 
+}
+
+?>
---
+++ app/presenters/homePresenter.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of homePresenter
+ *
+ * @author dominika
+ */
+class homePresenter {
+    // vytvorit instanciu modelu
+    dibi::query("SELECT 1")->fetchAll();
+}
+
+?>
