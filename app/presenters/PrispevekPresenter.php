50e73ca99daed0aae1eb7eaba5958f7c1a57aaec
fabcinova <fabcinovad@gmail.com>
2013-12-04 23:25:26 +0100

    nefunguju prispevky :(

--- app/presenters/PrispevekPresenter.php
+++ app/presenters/PrispevekPresenter.php
@@ -12,4 +12,57 @@ class PrispevekPresenter extends BasePresenter {
     {
         $this->template->prispevky = Prispevek::loadAll();
     }
+    
+    public function actionCreate()
+    {
+
+    }
+    
+    public function createComponentCreateForm()
+    {        
+        $leky = Lek::loadAll(); // vracia pole instancii objektu lek
+        $count_leky = count($leky);
+
+        $array = array();
+        
+        for($index = 1; $index <= $count_leky; $index++)
+        {
+            $leky[$index]->ID => $leky[$index]->nazev; //kktina, ktora nefachci
+        }
+        
+        $form = new Form();
+        
+        $form->addSelect("lek", "Lék",
+                array(
+                    $leky[$index]->ID => $leky[$index]->nazev
+                ));
+        $form->addSelect("pojistovna", "Pojišťovna",
+            array(
+                '201' => 'ČPZP',
+                '203' => 'VOZP',
+            ));
+        $form['pojistovna']->setDefaultValue('203');
+        $form->addText("vyse_prispevku", "Výše příspěvku");
+        $form->addText("platnost_od", "Platnost od");
+        $form->addText("platnost_do", "Platnost do");
+        $form->addSubmit("odeslat", "Uložit");
+        $form->onSuccess[] = callback($this, "create");
+        
+        return $form;
+    }
+    
+    public function create(Form $form)
+    {
+        $values = $form->getValues();
+        try
+        {
+            Prispevek::create($values->lek, $values->pojistovna, $values->vyse_prispevku, $values->platnost_od, $values->platnost_do);
+        }
+        catch(Exception $ex)
+        {
+            $this->flashMessage("Chyba: Příspěvek nebyl vložen!", "error");
+        }
+        $this->flashMessage("Uloženo.", "info");
+        $this->redirect("Prispevek:default");
+    }
 }
---
+++ temp/cache/_Nette.FileTemplate/_Prispevek.create.latte-2c2516b3fd79f2295d3f54e566a7fe96.php
@@ -0,0 +1,46 @@
+<?php //netteCache[01]000382a:2:{s:4:"time";s:21:"0.57208900 1386192821";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:60:"D:\git_projects\lekarna\app\templates\Prispevek\create.latte";i:2;i:1386192811;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:30:"80a7e46 released on 2013-08-08";}}}?><?php
+
+// source file: D:\git_projects\lekarna\app\templates\Prispevek\create.latte
+
+?><?php
+// prolog Nette\Latte\Macros\CoreMacros
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'q64jexdb96')
+;
+// prolog Nette\Latte\Macros\UIMacros
+
+// snippets support
+if (!empty($_control->snippetMode)) {
+	return Nette\Latte\Macros\UIMacros::renderSnippets($_control, $_l, get_defined_vars());
+}
+
+//
+// main template
+//
+Nette\Latte\Macros\FormMacros::renderFormBegin($form = $_form = (is_object("createForm") ? "createForm" : $_control["createForm"]), array()) ?>
+<table>
+<tr class="required">
+    <th><?php $_input = is_object("lek") ? "lek" : $_form["lek"]; if ($_label = $_input->getLabel()) echo $_label->addAttributes(array()) ?></th>
+    <td><?php $_input = (is_object("lek") ? "lek" : $_form["lek"]); echo $_input->getControl()->addAttributes(array()) ?></td>
+</tr>
+<tr class="required">
+    <th><?php $_input = is_object("pojistovna") ? "pojistovna" : $_form["pojistovna"]; if ($_label = $_input->getLabel()) echo $_label->addAttributes(array()) ?></th>
+    <td><?php $_input = (is_object("pojistovna") ? "pojistovna" : $_form["pojistovna"]); echo $_input->getControl()->addAttributes(array()) ?></td>
+</tr>
+<tr>
+    <th><?php $_input = is_object("vyse_prispevku") ? "vyse_prispevku" : $_form["vyse_prispevku"]; if ($_label = $_input->getLabel()) echo $_label->addAttributes(array()) ?></th>
+    <td><?php $_input = (is_object("vyse_prispevku") ? "vyse_prispevku" : $_form["vyse_prispevku"]); echo $_input->getControl()->addAttributes(array()) ?></td>
+</tr>
+<tr>
+    <th><?php $_input = is_object("platnost_od") ? "platnost_od" : $_form["platnost_od"]; if ($_label = $_input->getLabel()) echo $_label->addAttributes(array()) ?></th>
+    <td><?php $_input = (is_object("platnost_od") ? "platnost_od" : $_form["platnost_od"]); echo $_input->getControl()->addAttributes(array()) ?></td>
+</tr>
+<tr>
+    <th><?php $_input = is_object("platnost_do") ? "platnost_do" : $_form["platnost_do"]; if ($_label = $_input->getLabel()) echo $_label->addAttributes(array()) ?></th>
+    <td><?php $_input = (is_object("platnost_do") ? "platnost_do" : $_form["platnost_do"]); echo $_input->getControl()->addAttributes(array()) ?></td>
+</tr>
+<tr>
+    <th>&nbsp;</th>
+    <td><?php $_input = (is_object("odeslat") ? "odeslat" : $_form["odeslat"]); echo $_input->getControl()->addAttributes(array()) ?></td>
+</tr>
+</table>
+<?php Nette\Latte\Macros\FormMacros::renderFormEnd($_form) ;
